1f7aa125a060ebdcd08f64133bd62fa2913f354d
Erik Bjäreholt <erik@bjareho.lt>
2018-04-14 18:37:56 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import re
-from datetime import datetime
+import typing as T
+from copy import copy
+from collections import namedtuple
+from datetime import date, time, datetime
 
 
 re_time = re.compile(r"[0-9]{1,2}:[0-9]{1,2}")
 re_amount = re.compile(r"~?[0-9\.]+(k|d|m|mcg|u|n)?(l|g|IU)?")
 
+Event = namedtuple("Event", ["timestamp", "type", "data"])
+
+
+def parse_data(data: str):
+    datas = []
+    if re_amount.match(data):
+        for entry in (e.strip() for e in data.split("+")):
+            amount = re_amount.match(entry)[0]
+            # print(m)
+            datas.append({"raw": entry, "amount": amount})
+            # print(f"  - {entry}")
+    return datas
+
 
 def parse(text: str):
+    events = []  # type: T.List[Event]
+
     current_date = None
     for line in text.split("\n"):
         line = line.strip()
         if line:
             if line[0] == "#":
                 current_date = datetime.strptime(line[1:].strip(), "%Y-%m-%d")
-                print(current_date)
             elif re_time.match(line):
-                time = line.split("-")[0].strip()
+                t = datetime.strptime(line.split("-")[0].strip(), "%H:%M").time()
+                timestamp = copy(current_date).replace(hour=t.hour, minute=t.minute, second=t.second)
                 data = "-".join(line.split("-")[1:]).strip()
                 if re_amount.match(data):
                     # Data entry
-                    print(f"[data@{time}]   \t{data}")
-                    for entry in (e.strip() for e in data.split("+")):
-                        # print(f"  - {entry}")
-                        pass
+                    for entry in parse_data(data):
+                        events.append(Event(timestamp, "data", entry))
                 else:
                     # Journal entry
-                    print(f"[journal@{time}]\t{data}")
-
+                    events.append(Event(timestamp, "journal", data))
             elif line:
                 print(f"Couldn't identify line-type: \n> {line}")
+
+    for e in events:
+        print(f"{e.timestamp.isoformat()} [{e.type}]   \t{e.data}")
 
 
 test1 = """
