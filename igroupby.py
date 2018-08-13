0551627a30360c4107749aedbb788dab40ac380f
Erik Bjäreholt <erik@bjareho.lt>
2018-08-13 16:09:55 +0200

    added tests for igroupby

--- igroupby.py
+++ igroupby.py
@@ -9,5 +9,15 @@ def igroupby(l: List[T], key: Callable[[T], K]=lambda x: x) -> Dict[K, List[T]]:
     return {k: list(v) for k, v in groupby(sorted(l, key=key), key=key)}
 
 
-result1: Dict[int, List[float]] = igroupby([1.2, 1.5, 2], key=lambda x: round(x))
-result2: Dict[int, List[int]] = igroupby([1, 1, 2], key=lambda x: round(x))
+def test_igroupby() -> None:
+    d0: Dict[int, List[int]] = igroupby([1, 1, 2])
+    assert len(d0[1]) == 2
+    assert len(d0[2]) == 1
+
+    d1: Dict[int, List[float]] = igroupby([1.2, 1.5, 2], key=lambda x: round(x))
+    assert len(d1[1]) == 1
+    assert len(d1[2]) == 2
+
+    d2: Dict[int, List[int]] = igroupby([1, 1, 2], key=lambda x: round(x))
+    assert len(d2[1]) == 2
+    assert len(d2[2]) == 1
