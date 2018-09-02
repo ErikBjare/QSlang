c16ef839e33e45ed76d2f48034869794e004d31f
Erik Bjäreholt <erik@bjareho.lt>
2018-09-02 18:29:45 +0200

    added a preprocessing step for alcohol

--- main.py
+++ main.py
@@ -245,6 +245,33 @@ def _filter_events(events, start, end, substances):
     return events
 
 
+_alcohol_conc_assumptions = {
+    "gin": 0.4,
+    "vodka": 0.4,
+    "whiskey": 0.4,
+    "beer": 0.05,
+    "wine": 0.12,
+}
+
+
+def _alcohol_preprocess(events: List[Event]) -> List[Event]:
+    for e in events:
+        if not e.substance or not e.dose:
+            continue
+        if 'Alcohol' in e.tags:
+            conc_str = e.data.get("concentration", None)
+            if conc_str:
+                conc = 0.01 * float(conc_str.strip("%"))
+            elif e.substance.lower() in _alcohol_conc_assumptions:
+                conc = _alcohol_conc_assumptions[e.substance.lower()]
+            else:
+                print(f"Concentration unknown for event: {e}")
+                continue
+            e.data["substance"] = "Alcohol"
+            e.data["amount"] = _fmt_amount(e.dose.amount * conc, e.dose.unit)
+    return events
+
+
 def main():
     msgcounter = MsgCounterHandler()
 
@@ -256,6 +283,7 @@ def main():
 
     events = load_events()
     events = _filter_events(events, args.start, args.end, args.substances)
+    events = _alcohol_preprocess(events)
 
     if args.subcommand == "events":
         print_events(events)
