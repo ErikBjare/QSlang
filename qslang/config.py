cadf917b6e9aa3b52e46234dbf56d0b1792f4f40
Erik Bjäreholt <erik@bjareho.lt>
2021-07-14 15:37:20 +0200

    fix: fall back to example config if no real config found

--- qslang/config.py
+++ qslang/config.py
@@ -1,13 +1,20 @@
 import os
+import logging
+from pathlib import Path
 
 import toml
 
+logger = logging.getLogger(__name__)
 
-base_dir = os.path.dirname(__file__)
+rootdir = Path(__file__).resolve().parent
 
 
 def load_config():
-    with open(os.path.dirname(base_dir) + "/config.toml", "r") as f:
+    filepath = rootdir / "config.toml"
+    if not filepath.exists():
+        logger.warning("No config found, falling back to example config")
+        filepath = Path(rootdir) / "config.toml.example"
+    with open(filepath, "r") as f:
         return toml.load(f)
 
 
